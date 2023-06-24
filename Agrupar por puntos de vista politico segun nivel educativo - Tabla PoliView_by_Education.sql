c5e9b0086b5075f7ee93432c81ba3393a41d1cdc
Yolanda-17 <131808869+Yo-17@users.noreply.github.com>
2023-06-23 20:45:08 -0600

    Tabla PoliView_by_Education
    1 Query: Agrupar por puntos de vista politico segun nivel educativo

--- Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education.sql
+++ Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education.sql
@@ -1,33 +1,4 @@
 USE CAH;
-SHOW tables;
-
-Select * from CAH_table01;
-
-#Revisar que toda la informacion se subio correctamente
-Select Count(*) from CAH_table01;
-
-#Crear una tabla que contenga a todos los participantes que se identifican como mujeres
-CREATE TABLE Female_Participants AS 
-  SELECT *
-  FROM CAH_table01
-  WHERE 4_Gender = 'Woman';
-  
-  Select Count(*) from Female_Participants;
-  
-  #Crear una tabla que contenga a todos los participantes que se identifican como hombres
-CREATE TABLE Male_Participants AS 
-  SELECT *
-  FROM CAH_table01
-  WHERE 4_Gender = 'Man';
-  
-  Select Count(*) from Male_Participants;
-  
-  Select Count(*) From CAH_table01 WHERE 5_PoliticalParty = 'Democrat';
-  
-  #Crear una tabla donde se agrupen los participantes por political party 
-  CREATE TABLE Political_Party_Frequency AS
-  SELECT Count(5_PoliticalParty) AS Total 
-  FROM CAH_table01 group by 5_PoliticalParty;
 
 ## 1 Query: Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education
 SELECT * FROM CAH_table01;
@@ -51,5 +22,3 @@ FROM some_high_school
 LEFT JOIN high_school ON some_high_school.7_PoliticalViews = high_school.7_PoliticalViews
 LEFT JOIN grad_degree ON some_high_school.7_PoliticalViews = grad_degree.7_PoliticalViews
 LEFT JOIN bach_degree ON some_high_school.7_PoliticalViews = bach_degree.7_PoliticalViews;
-
-
