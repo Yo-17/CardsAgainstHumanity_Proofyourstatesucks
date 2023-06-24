bb7316e142a817abb63cca40cb8314bb188505df
Yolanda-17 <131808869+Yo-17@users.noreply.github.com>
2023-06-23 22:17:00 -0600

    Tabla AbortionViews_by_Education
    Agrupar por grado de educación segun perspectiva del abort

--- CAH Proyecto Modulo 1.sql
+++ CAH Proyecto Modulo 1.sql
@@ -31,7 +31,7 @@ CREATE TABLE Male_Participants AS
   SELECT Count(PoliticalParty) AS Total 
   FROM CAH_table01 group by PoliticalParty;
 
-## 1 Query: Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education
+## Query 2: Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education
 SELECT * FROM CAH_table01;
 
 CREATE TABLE some_high_school AS
@@ -52,3 +52,28 @@ FROM some_high_school
 LEFT JOIN high_school ON some_high_school.PoliticalViews = high_school.PoliticalViews
 LEFT JOIN grad_degree ON some_high_school.PoliticalViews = grad_degree.PoliticalViews
 LEFT JOIN bach_degree ON some_high_school.PoliticalViews = bach_degree.PoliticalViews;
+
+## Query 2:  Agrupar por grado de educación segun perspectiva del aborto - Tabla AbortionViews_by_Education
+
+#DROP TABLE Education;
+
+CREATE TEMPORARY TABLE some_high_school AS
+SELECT AbortionViews, count(ParticipantID) AS some_high_school_count FROM CAH_table01 WHERE education = "Some high school" GROUP BY AbortionViews;
+
+CREATE TEMPORARY TABLE high_school AS
+SELECT AbortionViews, count(ParticipantID) AS high_school_count FROM CAH_table01 WHERE education = "High school or high school equivalent" GROUP BY AbortionViews;
+
+CREATE TEMPORARY TABLE grad_degree AS
+SELECT AbortionViews, count(ParticipantID) AS grad_degree_count FROM CAH_table01 WHERE education = "Graduate degree" GROUP BY AbortionViews;
+
+CREATE TEMPORARY TABLE bach_degree AS
+SELECT AbortionViews, count(ParticipantID) AS bach_degree_count FROM CAH_table01 WHERE education = "Bachelor's degree or equivalent" GROUP BY AbortionViews;
+
+CREATE TABLE AbortionView_by_Education AS
+SELECT some_high_school.AbortionViews, some_high_school.some_high_school_count, high_school.high_school_count, grad_degree.grad_degree_count, bach_degree.bach_degree_count
+FROM some_high_school
+LEFT JOIN high_school ON some_high_school.AbortionViews = high_school.AbortionViews
+LEFT JOIN grad_degree ON some_high_school.AbortionViews = grad_degree.AbortionViews
+LEFT JOIN bach_degree ON some_high_school.AbortionViews = bach_degree.AbortionViews;
+
+SELECT * FROM AbortionView_by_Education;
