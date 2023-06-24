ee3231acdf4313ba66fa490ca87d5d2bbd2d367e
Yolanda-17 <131808869+Yo-17@users.noreply.github.com>
2023-06-23 21:21:58 -0600

    Deleting numbers from column names
    Column names containing numbers were creating issues when calling table.column

--- CAH Proyecto Modulo 1.sql
+++ CAH Proyecto Modulo 1.sql
@@ -1,6 +1,8 @@
 USE CAH;
 SHOW tables;
 
+DROP TABLE IF EXISTS Female_Participants, grad_degree, bach_degree, high_school, Male_Participants, Political_Party_frequency, PoliView_by_Education, some_high_school;
+
 Select * from CAH_table01;
 
 #Revisar que toda la informacion se subio correctamente
@@ -10,7 +12,7 @@ Select Count(*) from CAH_table01;
 CREATE TABLE Female_Participants AS 
   SELECT *
   FROM CAH_table01
-  WHERE 4_Gender = 'Woman';
+  WHERE Gender = 'Woman';
   
   Select Count(*) from Female_Participants;
   
@@ -18,15 +20,35 @@ CREATE TABLE Female_Participants AS
 CREATE TABLE Male_Participants AS 
   SELECT *
   FROM CAH_table01
-  WHERE 4_Gender = 'Man';
+  WHERE Gender = 'Man';
   
   Select Count(*) from Male_Participants;
   
-  Select Count(*) From CAH_table01 WHERE 5_PoliticalParty = 'Democrat';
+  Select Count(*) From CAH_table01 WHERE PoliticalParty = 'Democrat';
   
   #Crear una tabla donde se agrupen los participantes por political party 
   CREATE TABLE Political_Party_Frequency AS
-  SELECT Count(5_PoliticalParty) AS Total 
-  FROM CAH_table01 group by 5_PoliticalParty;
+  SELECT Count(PoliticalParty) AS Total 
+  FROM CAH_table01 group by PoliticalParty;
 
-#TESTCHANGE
+## 1 Query: Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education
+SELECT * FROM CAH_table01;
+
+CREATE TABLE some_high_school AS
+SELECT PoliticalViews, count(ParticipantID) AS some_high_school_count FROM CAH_table01 WHERE education = "Some high school" GROUP BY PoliticalViews;
+
+CREATE TABLE high_school AS
+SELECT PoliticalViews, count(ParticipantID) AS high_school_count FROM CAH_table01 WHERE education = "High school or high school equivalent" GROUP BY PoliticalViews;
+
+CREATE TABLE grad_degree AS
+SELECT PoliticalViews, count(ParticipantID) AS grad_degree_count FROM CAH_table01 WHERE education = "Graduate degree" GROUP BY PoliticalViews;
+
+CREATE TABLE bach_degree AS
+SELECT PoliticalViews, count(ParticipantID) AS bach_degree_count FROM CAH_table01 WHERE education = "Bachelor's degree or equivalent" GROUP BY PoliticalViews;
+
+CREATE TABLE PoliView_by_Education AS
+SELECT some_high_school.PoliticalViews, some_high_school.some_high_school_count, high_school.high_school_count, grad_degree.grad_degree_count, bach_degree.bach_degree_count
+FROM some_high_school
+LEFT JOIN high_school ON some_high_school.PoliticalViews = high_school.PoliticalViews
+LEFT JOIN grad_degree ON some_high_school.PoliticalViews = grad_degree.PoliticalViews
+LEFT JOIN bach_degree ON some_high_school.PoliticalViews = bach_degree.PoliticalViews;
---
+++ Cambio de nombre de columnas.sql
@@ -0,0 +1,71 @@
+#Cambiar nombres de columnas en Data Set
+ALTER TABLE CAH_table01
+RENAME COLUMN 1_Age TO Age;
+
+ALTER TABLE CAH_table01
+RENAME COLUMN 2_State TO State;
+
+ALTER TABLE CAH_table01
+RENAME COLUMN 3_Race TO Race;
+
+ALTER TABLE CAH_table01
+RENAME COLUMN 3_Race_Other TO Race_Other;
+
+ALTER TABLE CAH_table01
+RENAME COLUMN 4_Gender TO Gender;
+
+ALTER TABLE CAH_table01
+RENAME COLUMN 4_Gender_Other TO Gender_Other,
+RENAME COLUMN 5_PoliticalParty TO PoliticalParty;
+
+ALTER TABLE CAH_table01
+RENAME COLUMN 7_PoliticalViews TO PoliticalViews,
+RENAME COLUMN 7_PoliticalViews_Other TO PoliticalViews_Other,
+RENAME COLUMN 8_Religiosity TO Religiosity,
+RENAME COLUMN 9_ElectionViews TO ElectionViews,
+RENAME COLUMN 10_SepChurchState TO SepChurchState,
+RENAME COLUMN 11_AbortionViews TO AbortionViews,
+RENAME COLUMN 12_DeathPenaltyViews TO DeathPenaltyViews,
+RENAME COLUMN 13_DoctorPenalty TO DoctorPenalty,
+RENAME COLUMN 14_PatientPenalty TO PatientPenalty,
+RENAME COLUMN 15_CourtApproval TO CourtApproval,
+RENAME COLUMN 16_Approval TO Approval,
+RENAME COLUMN 17_ExceptFetalDeath TO ExceptFetalDeath,
+RENAME COLUMN 18_ExceptRapeIncest TO ExceptRapeIncest,
+RENAME COLUMN 19_ExceptMomsLife_ TO ExceptMomsLife,
+RENAME COLUMN 20_EmbryoisPerson TO EmbryoisPerson,
+RENAME COLUMN 21_AbortionCasualSex TO AbortionCasualSex,
+RENAME COLUMN 22_FourTrimesters TO FourTrimesters,
+RENAME COLUMN 23_StateLawViews TO StateLawViews,
+RENAME COLUMN 24_StateRestrict TO StateRestrict,
+RENAME COLUMN 25_StateExceptFetus TO StateExceptFetus,
+RENAME COLUMN 26_StateExceptRape TO StateExceptRape,
+RENAME COLUMN 27_StateExceptMom TO StateExceptMom,
+RENAME COLUMN 28_Condoms TO Condoms,
+RENAME COLUMN 29_Pill TO Pill,
+RENAME COLUMN 30_PullingOut TO PullingOut,
+RENAME COLUMN 31_ImplantFail TO ImplantFail,
+RENAME COLUMN 32_OutsideImplant TO OutsideImplant,
+RENAME COLUMN 33_Miscarriage TO Miscarriage,
+RENAME COLUMN 34_EctopicSurvive TO EctopicSurvive,
+RENAME COLUMN 35_EctopicSurgery TO EctopicSurgery,
+RENAME COLUMN 36_AssaultResponse TO AssaultResponse,
+RENAME COLUMN 37_FetalWeight TO FetalWeight,
+RENAME COLUMN 38_FetalLength TO FetalLength,
+RENAME COLUMN 39_LabelOvary TO LabelOvary,
+RENAME COLUMN 40_LabelUterus TO LabelUterus,
+RENAME COLUMN 41_LabelVagina TO LabelVagina,
+RENAME COLUMN 42_LabelClitoris TO LabelClitoris,
+RENAME COLUMN 43_SelectIUD TO SelectIUD,
+RENAME COLUMN 44_AgeofConsent TO AgeofConsent,
+RENAME COLUMN 45_CasualSex TO CasualSex,
+RENAME COLUMN 46_WomanCasualSex TO WomanCasualSex,
+RENAME COLUMN 47_EmbryosLab TO EmbryosLab,
+RENAME COLUMN 48_AbortionTrans TO AbortionTrans,
+RENAME COLUMN 49_OtherThoughts TO OtherThoughts;
+
+
+  
+
+  
+  
