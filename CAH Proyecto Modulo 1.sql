79b188e9d39fd0f2a282f5da8168e203565a08a8
Yolanda-17 <131808869+Yo-17@users.noreply.github.com>
2023-06-27 01:34:23 -0600

    Vistas
    Vistas y revisión final

--- CAH Proyecto Modulo 1.sql
+++ CAH Proyecto Modulo 1.sql
@@ -32,19 +32,19 @@ CREATE TABLE Male_Participants AS
   FROM CAH_table01 group by PoliticalParty;
 
 ## 1 Query: Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education
-SELECT * FROM CAH_table01;
-DROP TABLE some_high_school;
+#SELECT * FROM CAH_table01;
+#DROP TABLE some_high_school;
 
-CREATE TABLE some_high_school AS
+CREATE TEMPORARY TABLE some_high_school AS
 SELECT PoliticalViews, count(ParticipantID) AS some_high_school_count FROM CAH_table01 WHERE Education = "Some high school" GROUP BY PoliticalViews;
 
-CREATE TABLE high_school AS
+CREATE TEMPORARY TABLE high_school AS
 SELECT PoliticalViews, count(ParticipantID) AS high_school_count FROM CAH_table01 WHERE Education = "High school or high school equivalent" GROUP BY PoliticalViews;
 
-CREATE TABLE grad_degree AS
+CREATE TEMPORARY TABLE grad_degree AS
 SELECT PoliticalViews, count(ParticipantID) AS grad_degree_count FROM CAH_table01 WHERE Education = "Graduate degree" GROUP BY PoliticalViews;
 
-CREATE TABLE bach_degree AS
+CREATE TEMPORARY TABLE bach_degree AS
 SELECT PoliticalViews, count(ParticipantID) AS bach_degree_count FROM CAH_table01 WHERE Education = "Bachelor's degree or equivalent" GROUP BY PoliticalViews;
 
 CREATE TABLE PoliView_by_Education AS
@@ -54,30 +54,8 @@ LEFT JOIN high_school ON some_high_school.PoliticalViews = high_school.Political
 LEFT JOIN grad_degree ON some_high_school.PoliticalViews = grad_degree.PoliticalViews
 LEFT JOIN bach_degree ON some_high_school.PoliticalViews = bach_degree.PoliticalViews;
 
-## Query 2: Agrupar por puntos de vista politico segun nivel educativo - Tabla PoliView_by_Education
-SELECT * FROM CAH_table01;
-
-CREATE TEMPORARY TABLE some_high_school AS
-SELECT PoliticalViews, count(ParticipantID) AS some_high_school_count FROM CAH_table01 WHERE education = "Some high school" GROUP BY PoliticalViews;
-
-CREATE TEMPORARY TABLE high_school AS
-SELECT PoliticalViews, count(ParticipantID) AS high_school_count FROM CAH_table01 WHERE education = "High school or high school equivalent" GROUP BY PoliticalViews;
-
-CREATE TEMPORARY TABLE grad_degree AS
-SELECT PoliticalViews, count(ParticipantID) AS grad_degree_count FROM CAH_table01 WHERE education = "Graduate degree" GROUP BY PoliticalViews;
-
-CREATE TEMPORARY TABLE bach_degree AS
-SELECT PoliticalViews, count(ParticipantID) AS bach_degree_count FROM CAH_table01 WHERE education = "Bachelor's degree or equivalent" GROUP BY PoliticalViews;
-
-CREATE TABLE PoliView_by_Education AS
-SELECT some_high_school.PoliticalViews, some_high_school.some_high_school_count, high_school.high_school_count, grad_degree.grad_degree_count, bach_degree.bach_degree_count
-FROM some_high_school
-LEFT JOIN high_school ON some_high_school.PoliticalViews = high_school.PoliticalViews
-LEFT JOIN grad_degree ON some_high_school.PoliticalViews = grad_degree.PoliticalViews
-LEFT JOIN bach_degree ON some_high_school.PoliticalViews = bach_degree.PoliticalViews;
 
 ## Query 2:  Agrupar por grado de educación segun perspectiva del aborto - Tabla AbortionViews_by_Education
-
 #DROP TABLE Education;
 
 CREATE TEMPORARY TABLE some_high_school AS
@@ -102,36 +80,45 @@ LEFT JOIN bach_degree ON some_high_school.AbortionViews = bach_degree.AbortionVi
 SELECT * FROM AbortionView_by_Education;
 
 ## Query 3: Agrupar por opinion de la separacion del Edo y la Iglesia segun afiliacion a partido politico - SepChurchState_by_PoliticalParty
-#Drop table Stop_Enforcing;
+#Drop table Enforce_Separation;
 
 #Create Enforce_Separation
-CREATE TABLE Enforce_Separation AS
+CREATE TEMPORARY TABLE Enforce_Separation AS
 SELECT PoliticalParty, count(ParticipantID) AS Enforce_Separation_count 
 FROM CAH_table01 WHERE SepChurchState = "The federal government should enforce separation of church and state." 
 GROUP BY PoliticalParty;
 
+CREATE VIEW Enforce_Separation_Percentage AS
 SELECT PoliticalParty, Enforce_Separation_count,
 round(((Enforce_Separation_count * 100) / temp.Enforce_Separation_countSUM),2) AS Enforce_Separation_Percentage
 FROM Enforce_Separation
 CROSS JOIN (SELECT SUM(Enforce_Separation_count) AS Enforce_Separation_countSUM FROM Enforce_Separation) temp;
 
 #Create Stop_Enforcing
-CREATE TABLE Stop_Enforcing AS
+CREATE TEMPORARY TABLE Stop_Enforcing AS
 SELECT PoliticalParty, count(ParticipantID) AS Stop_Enforcing_count 
 FROM CAH_table01 WHERE SepChurchState = "The federal government should stop enforcing separation of church and state." 
 GROUP BY PoliticalParty;
 
+CREATE VIEW Stop_Enforcing_Percentage AS
 SELECT PoliticalParty, Stop_Enforcing_count,
 round(((Stop_Enforcing_count * 100) / temp.Stop_Enforcing_countSUM),2) AS Stop_Enforcing_Percentage
 FROM Stop_Enforcing
 CROSS JOIN (SELECT SUM(Stop_Enforcing_count) AS Stop_Enforcing_countSUM FROM Stop_Enforcing) temp;
 
 # Table - SepChurchState_by_PoliticalParty 
-## Percentage columns are not included in this table, looking for solutions 
 CREATE TABLE SepChurchState_by_PoliticalParty AS
 SELECT Enforce_Separation.PoliticalParty, Enforce_Separation.Enforce_Separation_count, Stop_Enforcing.Stop_Enforcing_count
 FROM Enforce_Separation
 LEFT JOIN Stop_Enforcing ON Enforce_Separation.PoliticalParty = Stop_Enforcing.PoliticalParty;
+
+# View - sepchurchstate_by_politicalparty_percent
+CREATE VIEW SepChurchState_by_PoliticalParty_Percent AS
+SELECT Enforce_Separation_Percentage.PoliticalParty, Enforce_Separation_Percentage.Enforce_Separation_Percentage, Stop_Enforcing_Percentage.Stop_Enforcing_Percentage 
+FROM CAH.enforce_separation_percentage
+LEFT JOIN CAH.stop_enforcing_percentage ON Enforce_Separation_Percentage.PoliticalParty = Stop_Enforcing_Percentage.PoliticalParty;
+
+#SELECT * FROM SepChurchState_by_PoliticalParty;
 
 ##Query 4: Criminalizacion del Aborto - Opiniones por genero - PatientPenalty_by_Gender
 #Drop table penalty_female;
@@ -146,6 +133,8 @@ CREATE TABLE PatientPenalty_by_Gender AS
 SELECT penalty_female.PatientPenalty, penalty_female.f_penalty_count, penalty_male.m_penalty_count
 FROM penalty_female
 LEFT JOIN penalty_male ON penalty_female.PatientPenalty = penalty_male.PatientPenalty;
+
+SELECT * FROM PatientPenalty_by_Gender;
 
 ##Query 5: Organizar de mayor a menor los Estados con mayor numero de personas que aprueban la anulacion de la sentencia de Roe v. Wade - Yes_CourtApproval_by_State
 #Drop table Yes_CourtApproval_by_State;
@@ -208,3 +197,46 @@ FROM morally_wrong
 LEFT JOIN morally_justified ON morally_wrong.PoliticalViews = morally_justified.PoliticalViews;
 
 SELECT * FROM DeathPenaltyViews_by_PoliticalViews;
+
+##Vistas 
+#1. Death Penalty and Separation of Church and State by Political Views
+
+CREATE TEMPORARY TABLE Stop_Enforcing_by_view AS
+SELECT PoliticalViews, count(ParticipantID) AS Stop_Enforcing_count 
+FROM CAH_table01 WHERE SepChurchState = "The federal government should stop enforcing separation of church and state." 
+GROUP BY PoliticalViews;
+
+CREATE TEMPORARY TABLE Enforce_Separation_by_view AS
+SELECT PoliticalViews, count(ParticipantID) AS Enforce_Separation_count 
+FROM CAH_table01 WHERE SepChurchState = "The federal government should enforce separation of church and state." 
+GROUP BY PoliticalViews;
+
+CREATE TABLE SepChurchState_by_PoliticalViews AS
+SELECT Enforce_Separation_by_view.PoliticalViews, Enforce_Separation_by_view.Enforce_Separation_count, Stop_Enforcing_by_view.Stop_Enforcing_count
+FROM Enforce_Separation_by_view
+LEFT JOIN Stop_Enforcing_by_view ON Enforce_Separation_by_view.PoliticalViews = Stop_Enforcing_by_view.PoliticalViews;
+
+CREATE VIEW DeathPenalty_and_SepChurchState AS
+SELECT SepChurchState_by_PoliticalViews.PoliticalViews, SepChurchState_by_PoliticalViews.Enforce_Separation_count, SepChurchState_by_PoliticalViews.Stop_Enforcing_count, DeathPenaltyViews_by_PoliticalViews.morally_wrong_count, DeathPenaltyViews_by_PoliticalViews.morally_justified_count
+FROM SepChurchState_by_PoliticalViews
+LEFT JOIN DeathPenaltyViews_by_PoliticalViews ON SepChurchState_by_PoliticalViews.PoliticalViews = DeathPenaltyViews_by_PoliticalViews.PoliticalViews;
+
+SELECT * FROM CAH.deathpenalty_and_sepchurchstate;
+
+#2. Para comparar el número de personas que están de acuerdo con la derogación de Roe v. Wade y los que creen que las leyes en torno al aborto deberían ser mas permisivas y más restrictivas.
+
+CREATE VIEW Yes_CourtApproval_StateLaws AS
+SELECT Yes_CourtApproval_by_State.State, Yes_CourtApproval_by_State.Buu_RvW, StateLawViews_by_State.more_permissive_count, StateLawViews_by_State.more_restrictive_count
+FROM Yes_CourtApproval_by_State
+LEFT JOIN StateLawViews_by_State ON Yes_CourtApproval_by_State.State = StateLawViews_by_State.State;
+
+SELECT * FROM Yes_CourtApproval_StateLaws;
+
+#3. Filtrar el número de personas que están de acuerdo con la derogación de Roe v. Wade pero creen que las leyes entorno al aborto deberían ser más permisivas por estado.
+
+CREATE VIEW Yes_CourtApproval_More_Permissive AS
+SELECT State, COUNT(IF(CourtApproval = 'Yes', State, NULL)) AS Disagree_w_RoevWade
+FROM CAH_table01 WHERE StateLawViews = "My state's abortion laws should be more permissive." 
+GROUP BY State ORDER BY Disagree_w_RoevWade DESC;
+
+SELECT * FROM Yes_CourtApproval_More_Permissive;
